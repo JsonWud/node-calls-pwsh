e5f327d4e9935799086ee6c401009208c584b1ba
Jason Wood <json.wud@gmail.com>
2023-07-14 17:31:42 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// The script is running!
+console.log('|;|;|;|;|;|   Hello World  |;|;|;|;|;|`')
+// We will send this JSON object to the pwsh script
+testRequest = {
+    "prop1": "value1",
+    "prop2": "xyz",
+    "prop3": {
+        "array1": [
+            "a",
+            "b",
+            "c"
+        ]
+    }
+}
+// Use node's child_process to spawn a pwsh process to run a script
+const { spawn } = require('child_process');
+const requestJson = testRequest ? JSON.stringify(testRequest) : '';
+const child = spawn('pwsh', ['./script.ps1', requestJson], { windowsVerbatimArguments: true });
+child.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+});
+child.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
+child.on('close', (code) => {
+    console.log(`|;|;|;|;|;|   pwsh child process exited with code ${code}  |;|;|;|;|;|`);
+});
---
+++ script.ps1
@@ -0,0 +1,22 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$MyJsonPayload
+)
+# Always show this to confirm what we're receiving before conversion
+$myTestPattern = [ordered]@{
+    prop1 = 'value1'
+    prop2 = $MyArg
+    prop3 = @{
+        array1 = @('a', 'b', 'c')
+    }
+}
+$testPattern = $myTestPattern | ConvertTo-Json
+# Always show this to confirm we can add data and return a new object
+$MyInput = $MyJsonPayload | ConvertFrom-Json -AsHashtable
+$MyInput['status'] = 'success'
+$MyInput['answer'] = 42
+$MyInput['errors'] = $null
+$addDataToInput = $MyInput | ConvertTo-Json
+# Return the array of tests
+$testPattern, $addDataToInput
